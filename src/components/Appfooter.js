dd6146836c67c626f68057bab8f40a7451484eb4
= <56171682+hubertnare@users.noreply.github.com>
2021-11-27 00:56:12 +0200

    Update Appfooter.js

--- src/components/Appfooter.js
+++ src/components/Appfooter.js
@@ -1,14 +1,15 @@
 import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
+import Darkbutton from '../components/Darkbutton';
 
 class Appfooter extends Component {
     render() {
         return (
-            <div className="app-footer border-0 shadow-lg bg-primary-gradiant">
-                <Link to="/home" className="nav-content-bttn nav-center"><i className="feather-home"></i></Link>
-                <Link to="/defaultvideo" className="nav-content-bttn"><i className="feather-package"></i></Link>
-                <Link to="/defaultlive" className="nav-content-bttn" data-tab="chats"><i className="feather-layout"></i></Link>           
-                <Link to="/shop2" className="nav-content-bttn"><i className="feather-layers"></i></Link>
+            <div className="app-footer border-0 shadow-lg bg-woba-primary">
+                <Link to="/" className="nav-content-bttn nav-center"><i className="feather-home text-woba-secondary"></i></Link>
+                <Link to="/defaultMember" className="nav-content-bttn"><i className="feather-user text-woba-secondary"></i></Link>
+                <Link to="/defaultmessage" className="nav-content-bttn" data-tab="chats"><i className="feather-message-square text-woba-secondary"></i></Link>           
+                <Darkbutton />
                 <Link to="/defaultsettings" className="nav-content-bttn"><img src="assets/images/female-profile.png" alt="user" className="w30 shadow-xss" /></Link>
             </div>        
         );
