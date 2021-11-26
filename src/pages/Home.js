003cf5c080ebd75b27d920bd93a0a6285976aeca
= <56171682+hubertnare@users.noreply.github.com>
2021-11-27 00:58:48 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,6 @@
 import React, { Component, Fragment } from "react";
 
 import Header from '../components/Header';
-import Leftnav from '../components/Leftnav';
-import Rightchat from '../components/Rightchat';
 import Appfooter from '../components/Appfooter';
 import Popupchat from '../components/Popupchat';
 
@@ -25,8 +23,6 @@ class Home extends Component {
         return (
             <Fragment> 
                 <Header />
-                <Leftnav />
-                <Rightchat />
 
                 <div className="main-content right-chat-active">
                     <div className="middle-sidebar-bottom">
@@ -34,13 +30,13 @@ class Home extends Component {
                             <div className="row feed-body">
                                 <div className="col-xl-8 col-xxl-9 col-lg-8">
                                     <Createpost />
-                                    <Postview id="32" postvideo="" postimage="post.png" avater="user.png" user="Hubert Nare" time="12 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
-                                    <Postview id="31" postvideo="" postimage="post.png" avater="user.png" user="Tino Makomo" time="30 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
-                                    <Postview id="33" postvideo="" postimage="post.png" avater="user.png" user="Laz Muchenje" time="4 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                    <Postview id="32" postimage="post.png" avater="user.png" user="Hubert Nare" time="12 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                    <Postview id="31" postimage="post.png" avater="user.png" user="Tino Makomo" time="30 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                    <Postview id="33" postimage="post.png" avater="user.png" user="Laz Muchenje" time="4 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
                                     <Memberslider />
-                                    <Postview id="35" postvideo="" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="6 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                    <Postview id="35" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="6 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
                                     <Friendsilder />
-                                    <Postview id="36" postvideo="" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="1 day ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                    <Postview id="36" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="1 day ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
                                     <Load />
                                 </div>
                                 <div className="col-xl-4 col-xxl-3 col-lg-4 ps-lg-0">
