2f0e55a994d2aea2a31f4f3e91f7c3b31f52f7aa
= <56171682+hubertnare@users.noreply.github.com>
2021-12-17 22:01:54 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 
+//core components
 import Header from '../components/Header';
 import Appfooter from '../components/Appfooter';
 import Popupchat from '../components/Popupchat';
@@ -18,43 +19,41 @@ import Profilephoto from '../components/Profilephoto';
 
 
 
-class Home extends Component {
-    render() {
-        return (
-            <Fragment> 
-                <Header />
+const Home = () => {
+    return (
+        <Fragment> 
+            <Header />
 
-                <div className="main-content right-chat-active">
-                    <div className="middle-sidebar-bottom">
-                        <div className="middle-sidebar-left">
-                            <div className="row feed-body">
-                                <div className="col-xl-8 col-xxl-9 col-lg-8">
-                                    <Createpost />
-                                    <Postview id="32" postimage="post.png" avater="user.png" user="Hubert Nare" time="12 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
-                                    <Postview id="31" postimage="post.png" avater="user.png" user="Tino Makomo" time="30 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
-                                    <Postview id="33" postimage="post.png" avater="user.png" user="Laz Muchenje" time="4 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
-                                    <Memberslider />
-                                    <Postview id="35" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="6 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
-                                    <Friendsilder />
-                                    <Postview id="36" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="1 day ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
-                                    <Load />
-                                </div>
-                                <div className="col-xl-4 col-xxl-3 col-lg-4 ps-lg-0">
-                                    <Friends />
-                                    <Contacts />
-                                    <Group />
-                                    <Events />
-                                    <Profilephoto />
-                                </div>
+            <div className="main-content right-chat-active">
+                <div className="middle-sidebar-bottom">
+                    <div className="middle-sidebar-left">
+                        <div className="row feed-body">
+                            <div className="col-xl-8 col-xxl-9 col-lg-8">
+                                <Createpost />
+                                <Postview id="32" postimage="post.png" avater="user.png" user="Hubert Nare" time="12 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                <Postview id="31" postimage="post.png" avater="user.png" user="Tino Makomo" time="30 min ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                <Postview id="33" postimage="post.png" avater="user.png" user="Laz Muchenje" time="4 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                <Memberslider />
+                                <Postview id="35" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="6 hours ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                <Friendsilder />
+                                <Postview id="36" postimage="post.png" avater="user.png" user="Tendai Chimbetete" time="1 day ago" des="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus." />
+                                <Load />
+                            </div>
+                            <div className="col-xl-4 col-xxl-3 col-lg-4 ps-lg-0">
+                                <Friends />
+                                <Contacts />
+                                <Group />
+                                <Events />
+                                <Profilephoto />
                             </div>
                         </div>
                     </div>
                 </div>
-                <Popupchat />
-                <Appfooter /> 
-            </Fragment>
-        );
-    }
+            </div>
+            <Popupchat />
+            <Appfooter /> 
+        </Fragment>
+    );
 }
 
 export default Home;
